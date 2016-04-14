e9ca7c28b1bf2cda951b591a9d945cdde8f2b454
David Thole <dthole@gmail.com>
2016-04-14 14:08:17 -0500

    Adding an intial table for 'People'

---
+++ libTestBed/Concrete/EFlibTestBed.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using libTestBed.Entities;
+
+namespace libTestBed.Concrete
+{
+    class EFLibTestBed : DbContext
+    {
+        public EFLibTestBed(string connString) : base(connString)
+        {
+            
+        }
+
+        public EFLibTestBed(DbConnection connection) : base(connection, true)
+        {
+            
+        }
+
+        public EFLibTestBed() : base()
+        {
+            
+        }
+
+        public virtual DbSet<People> People { get; set; }
+    }
+}
